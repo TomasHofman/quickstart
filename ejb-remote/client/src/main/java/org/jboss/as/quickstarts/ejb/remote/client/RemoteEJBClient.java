bfe2c16df4f935527d0fe413c84fe01c77051536
Tomas Hofman <thofman@redhat.com>
2021-01-14 19:10:58 +0100

    Test - EJB client connecting to two distinct clusters

--- ejb-remote/client/src/main/java/org/jboss/as/quickstarts/ejb/remote/client/RemoteEJBClient.java
+++ ejb-remote/client/src/main/java/org/jboss/as/quickstarts/ejb/remote/client/RemoteEJBClient.java
@@ -18,12 +18,23 @@ package org.jboss.as.quickstarts.ejb.remote.client;
 
 import org.jboss.as.quickstarts.ejb.remote.stateful.RemoteCounter;
 import org.jboss.as.quickstarts.ejb.remote.stateless.RemoteCalculator;
+import org.jboss.ejb.client.ClusterNodeSelector;
+import org.jboss.ejb.client.DeploymentNodeSelector;
+import org.jboss.ejb.client.EJBClientCluster;
+import org.jboss.ejb.client.EJBClientConnection;
+import org.jboss.ejb.client.EJBClientContext;
+import org.jboss.ejb.protocol.remote.RemoteTransportProvider;
+import org.jboss.logging.Logger;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.Hashtable;
+import java.util.stream.Collectors;
 
 /**
  * A sample program which acts a remote client for a EJB deployed on JBoss EAP server. This program shows how to lookup stateful and
@@ -33,14 +44,29 @@ import java.util.Hashtable;
  */
 public class RemoteEJBClient {
 
+    private static final Logger logger = Logger.getLogger(RemoteEJBClient.class);
+
     private static final String HTTP = "http";
 
     public static void main(String[] args) throws Exception {
-        // Invoke a stateless bean
-        invokeStatelessBean();
+        // cluster 1
+        URI cluster1Node1 = new URI("remote+http://localhost:8080");
+        URI cluster1Node2 = new URI("remote+http://localhost:8230");
 
-        // Invoke a stateful bean
-        invokeStatefulBean();
+        // cluster 2
+        URI cluster2Node1 = new URI("remote+http://localhost:8330");
+        URI cluster2Node2 = new URI("remote+http://localhost:8430");
+
+        // Invoke a stateless bean on cluster 1
+        setEjbClientContext(cluster1Node1, cluster1Node2);
+        invokeStatelessBean("ejb-remote-server-side");
+//        invokeStatefulBean();
+
+        // Invoke a stateless bean on cluster 2
+        setEjbClientContext(cluster2Node1, cluster2Node2);
+        invokeStatelessBean("ejb-remote-server-side2");
+//        invokeStatefulBean();
+
     }
 
     /**
@@ -48,9 +74,9 @@ public class RemoteEJBClient {
      *
      * @throws NamingException
      */
-    private static void invokeStatelessBean() throws NamingException {
+    private static void invokeStatelessBean(String appName) throws NamingException {
         // Let's lookup the remote stateless calculator
-        final RemoteCalculator statelessRemoteCalculator = lookupRemoteStatelessCalculator();
+        final RemoteCalculator statelessRemoteCalculator = lookupRemoteStatelessCalculator(appName);
         System.out.println("Obtained a remote stateless calculator for invocation");
         // invoke on the remote calculator
         int a = 204;
@@ -101,22 +127,48 @@ public class RemoteEJBClient {
         }
     }
 
+    private static void setEjbClientContext(URI... uris) {
+        EJBClientContext.Builder builder = new EJBClientContext.Builder();
+
+        builder.addTransportProvider(new RemoteTransportProvider()); // we always have to add the RemoteTransportProvider
+
+        builder.setClusterNodeSelector(new CustomClusterNodeSelector());
+        builder.setDeploymentNodeSelector(new CustomDeploymentNodeSelector());
+        builder.setInvocationTimeout(30000); // set the invocation timeout to 2000ms
+        builder.setMaximumConnectedClusterNodes(2);
+
+        builder.addClientCluster(new EJBClientCluster.Builder()
+                .setName("ejb")
+                .build());
+
+        for (URI uri: uris) {
+            builder.addClientConnection(new EJBClientConnection.Builder()
+                    .setDestination(uri)
+                    .build());
+        }
+
+        EJBClientContext.getContextManager().setThreadDefault(builder.build());
+
+    }
+
     /**
      * Looks up and returns the proxy to remote stateless calculator bean
      *
      * @return
      * @throws NamingException
      */
-    private static RemoteCalculator lookupRemoteStatelessCalculator() throws NamingException {
+    private static RemoteCalculator lookupRemoteStatelessCalculator(String appName) throws NamingException {
         final Hashtable<String, String> jndiProperties = new Hashtable<>();
         jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
-        if(Boolean.getBoolean(HTTP)) {
+        /*
+        if (Boolean.getBoolean(HTTP)) {
             //use HTTP based invocation. Each invocation will be a HTTP request
-            jndiProperties.put(Context.PROVIDER_URL,"http://localhost:8080/wildfly-services");
+            jndiProperties.put(Context.PROVIDER_URL, "http://localhost:8080/wildfly-services");
         } else {
             //use HTTP upgrade, an initial upgrade requests is sent to upgrade to the remoting protocol
-            jndiProperties.put(Context.PROVIDER_URL,"remote+http://localhost:8080");
+            jndiProperties.put(Context.PROVIDER_URL, "remote+http://localhost:8080");
         }
+        */
         final Context context = new InitialContext(jndiProperties);
 
         // The JNDI lookup name for a stateless session bean has the syntax of:
@@ -138,8 +190,8 @@ public class RemoteEJBClient {
         // the whole package name.
 
         // let's do the lookup
-        return (RemoteCalculator) context.lookup("ejb:/ejb-remote-server-side/CalculatorBean!"
-            + RemoteCalculator.class.getName());
+        return (RemoteCalculator) context.lookup("ejb:/" + appName + "/CalculatorBean!"
+                + RemoteCalculator.class.getName());
     }
 
     /**
@@ -152,13 +204,15 @@ public class RemoteEJBClient {
         final Hashtable<String, String> jndiProperties = new Hashtable<>();
         //jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
         jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
-        if(Boolean.getBoolean(HTTP)) {
+        /*
+        if (Boolean.getBoolean(HTTP)) {
             //use HTTP based invocation. Each invocation will be a HTTP request
-            jndiProperties.put(Context.PROVIDER_URL,"http://localhost:8080/wildfly-services");
+            jndiProperties.put(Context.PROVIDER_URL, "http://localhost:8080/wildfly-services");
         } else {
             //use HTTP upgrade, an initial upgrade requests is sent to upgrade to the remoting protocol
-            jndiProperties.put(Context.PROVIDER_URL,"remote+http://localhost:8080");
+            jndiProperties.put(Context.PROVIDER_URL, "remote+http://localhost:8080");
         }
+        */
         final Context context = new InitialContext(jndiProperties);
 
         // The JNDI lookup name for a stateful session bean has the syntax of:
@@ -181,6 +235,34 @@ public class RemoteEJBClient {
 
         // let's do the lookup
         return (RemoteCounter) context.lookup("ejb:/ejb-remote-server-side/CounterBean!"
-            + RemoteCounter.class.getName() + "?stateful");
+                + RemoteCounter.class.getName() + "?stateful");
+    }
+
+    static class CustomClusterNodeSelector implements ClusterNodeSelector {
+
+        @Override
+        public String selectNode(String clusterName, String[] connectedNodes, String[] totalAvailableNodes) {
+            logger.infof("CustomClusterNodeSelector: Connected nodes: %s", String.join(", ", connectedNodes));
+            if (connectedNodes.length > 0) {
+                return connectedNodes[0];
+            } else if (totalAvailableNodes.length > 0) {
+                return totalAvailableNodes[0];
+            } else {
+                return null;
+            }
+        }
+    }
+
+    static class CustomDeploymentNodeSelector implements DeploymentNodeSelector {
+
+        @Override
+        public String selectNode(String[] eligibleNodes, String appName, String moduleName, String distinctName) {
+            logger.infof("CustomDeploymentNodeSelector: Eligible nodes: %s", String.join(", ", eligibleNodes));
+            if (eligibleNodes.length > 0) {
+                return eligibleNodes[0];
+            } else {
+                return null;
+            }
+        }
     }
 }
